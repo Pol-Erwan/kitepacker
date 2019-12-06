011ea7ed8c14147f9b70152bc1bf02f41dd66da3
Pol-Erwan <polerwankite@gmail.com>
2019-12-06 15:26:57 +0100

    nothing

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -1,13 +1,9 @@
 class AvatarsController < ApplicationController
 
   def create
+ 
     @user = User.find(params[:user_id])
-      if params[:avatar] == nil
-	redirect_to(edit_user_registration_path(@user))	
-      else
-        @user.avatar.attach(params[:avatar])
-        redirect_to(edit_user_registration_path(@user))
-      end
+    @user.avatar.attach(params[:avatar])
+    redirect_to(user_path(@user))
   end
-
 end
