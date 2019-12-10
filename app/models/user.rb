9837d96ac6f990b794748d69913047fe9e63e71f
Pol-Erwan <polerwankite@gmail.com>
2019-12-10 15:19:10 +0100

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,19 +12,19 @@ class User < ApplicationRecord
 
   after_create :welcome_send
 
-  def welcome_send
-    UserMailer.welcome_email(self).deliver_now
-  end
+#  def welcome_send
+#    UserMailer.welcome_email(self).deliver_now
+#  end
 
 
 
-  def self.new_with_session(params, session)
-  super.tap do |user|
-    if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-      user.email = data["email"] if user.email.blank?
-    end
-  end
-end
+#  def self.new_with_session(params, session)
+#  super.tap do |user|
+#    if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+#      user.email = data["email"] if user.email.blank?
+#    end
+#  end
+#end
 
 def self.from_omniauth(auth)
   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
